5fa1b92449d2827ba79a02091dee6426ddba349b
UiHyeon <hyunkim6790@gmail.com>
2024-11-08 01:05:36 +0900

    refactor(PurchaseDetailsValidator): 상품과 갯수 검증 함수 생성

--- src/main/kotlin/store/controller/ConvenienceController.kt
+++ src/main/kotlin/store/controller/ConvenienceController.kt
@@ -1,7 +1,7 @@
 package store.controller
 
-import camp.nextstep.edu.missionutils.DateTimes
 import store.model.FileManager
+import store.util.Vaildator.PurchaseDetailsValidator.getValidatePurchaseDetails
 import store.view.InputView
 import store.view.OutputView
 
@@ -9,20 +9,27 @@ class ConvenienceController(
     private val inputView: InputView = InputView(),
     private val outputView: OutputView = OutputView(),
 ){
-    private lateinit var fileManager: FileManager
+    private var fileManager: FileManager = FileManager()
 
     fun start() {
         val products = fileManager.readProductFile("src/main/resources/products.md")
         val promotion = fileManager.readPromotionFile("src/main/resources/promotions.md")
-        println(DateTimes.now())
+        // println(DateTimes.now())
 
         outputView.welcomeMessage()
         outputView.printProductFormat(products)
-        inputView.getPurchaseDetails()
+
+        val rawPurchaseDetails = inputView.getPurchaseDetails()
+        val validatedPurchaseList = getValidatePurchaseDetails(rawPurchaseDetails)
+
+        //println(validatedPurchaseList)
+
+
         inputView.selectPromotion()
         inputView.selectMembership()
 
         inputView.selectAdditionalPurchases()
 
     }
+
 }
--- src/main/kotlin/store/view/InputView.kt
+++ src/main/kotlin/store/view/InputView.kt
@@ -1,18 +1,18 @@
 package store.view
 
 import camp.nextstep.edu.missionutils.Console
-import store.util.constant.InputMessage
+import store.util.constant.Input
 
 class InputView {
     val regex = Regex("[\\[\\]]")
 
     fun getPurchaseDetails(): String {
-        println(InputMessage.PRODUCT_AND_QUANTITY)
+        println(Input.PRODUCT_AND_QUANTITY.message)
         return Console.readLine()
     }
 
     fun selectPromotion(): String {
-        println("현재 %s는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)")
+        println("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)")
         return Console.readLine()
     }
 
