900e86e70421a04862afa6ef16c374483dd446ed
UiHyeon <hyunkim6790@gmail.com>
2024-11-07 22:09:09 +0900

    feat(Output): 출력 상수 추가

--- src/main/kotlin/store/util/constant/Output.kt
+++ src/main/kotlin/store/util/constant/Output.kt
@@ -1,4 +1,15 @@
 package store.util.constant
 
-enum class Output {
+enum class Output(private val message: String) {
+    RECEIPT_HEADER("===========W 편의점============="),
+    RECEIPT_ITEM_LABEL("상품명\t\t수량\t금액"),
+    RECEIPT_PURCHASED_ITEM("%s\t\t%d\t%d"),
+    RECEIPT_PROMOTION_HEADER("===========증\t정============="),
+    RECEIPT_PROMOTION_ITEM("%s\t\t%d"),
+    RECEIPT_PROMOTION_FOOTER("=============================="),
+    RECEIPT_TOTAL_AMOUNT("총구매액\t\t%d\t%d"),
+    RECEIPT_EVENT_DISCOUNT("행사할인\t\t\t%d"),
+    RECEIPT_MEMBERSHIP_DISCOUNT("멤버십할인\t\t\t%d"),
+    RECEIPT_FINAL_AMOUNT("내실돈\t\t\t%d"),
+
 }
