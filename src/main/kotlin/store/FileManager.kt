329b613c0fd7b1e1804d2f5c1e858cf3ae408439
UiHyeon <hyunkim6790@gmail.com>
2024-11-07 14:58:29 +0900

    feat(FileManager): 프로모션 출력 확인

--- src/main/kotlin/store/FileManager.kt
+++ src/main/kotlin/store/FileManager.kt
@@ -2,16 +2,15 @@ package store
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.text.SimpleDateFormat
 import java.util.*
 
 class FileManager {
     fun readProductFile(filePath: String): List<Product> {
         val lines = Files.readAllLines(Paths.get(filePath))
-        val header = lines.first().split(",")
 
         return lines.drop(1).map { line ->
             val values = line.split(",")
-
             Product(
                 name = values[0],
                 price = values[1].toInt(),
@@ -23,17 +22,16 @@ class FileManager {
 
     fun readPromotionFile(filePath: String): List<Promotion> {
         val lines = Files.readAllLines(Paths.get(filePath))
-        val header = lines.first().split(",")
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
 
         return lines.drop(1).map { line ->
             val values = line.split(",")
-
             Promotion(
                 name = values[0],
                 buy = values[1].toInt(),
                 get = values[2].toInt(),
-                start_date = Date(values[3]),
-                end_date = Date(values[4])
+                start_date = dateFormat.parse(values[3]),
+                end_date = dateFormat.parse(values[4])
             )
         }
     }
