bf524368e09d7e23bb26f60b7057e598a0c4d90b
김의현 <hyunkim6790@gmail.com>
2024-11-30 17:35:48 +0900

    feat(Promotion): 프로모션 데이터 클래스 구현

--- src/main/kotlin/store/Promotion.kt
+++ src/main/kotlin/store/Promotion.kt
@@ -2,4 +2,10 @@ package store
 
 import java.util.Date
 
-data class Promotion(val name:String, val buy:Int, val get:Int, val start_date: Date, val end_date: Date)
+data class Promotion(
+    val name:String,
+    val buy:Int,
+    val get:Int,
+    val start_date: Date,
+    val end_date: Date
+)
