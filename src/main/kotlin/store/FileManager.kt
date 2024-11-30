0643d58047b1816f1a0b69fdf8c2f2de1274322a
김의현 <hyunkim6790@gmail.com>
2024-11-30 22:19:34 +0900

    feat(FileManager): 프로모션 불러오는 함수 구현

--- src/main/kotlin/store/FileManager.kt
+++ src/main/kotlin/store/FileManager.kt
@@ -1,32 +1,36 @@
 package store
 
-import java.nio.file.Files
-import java.nio.file.Paths
+import java.io.File
+import java.text.SimpleDateFormat
+import java.time.LocalDate
 
-fun readProductsCsv(filePath: String): List<Products> {
-    val lines = Files.readAllLines(Paths.get(filePath))
-    return lines.drop(1).map { line ->
-        val values = line.split(",")
-        Products(
-            name = values[0],
-            price = values[1].toInt(),
-            quantity = values[2].toInt(),
-            promotion = if (values[3] == "null") "" else values[3]
-        )
+object FileManager {
+    fun loadProductsCsv(filePath: String): List<Products> {
+        return File(filePath).readLines().drop(1).map { line ->
+            val parts = line.split(",")
+            Products(
+                name = parts[0],
+                price = parts[1].toInt(),
+                quantity = parts[2].toInt(),
+                promotion = if (parts[3] == "null") "" else parts[3]
+            )
+        }
+    }
+
+    fun loadPromotionCsv(filePath: String): List<Promotion> {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+        return File(filePath).readLines().drop(1).map { line ->
+            val parts = line.split(",")
+            Promotion(
+                name = parts[0],
+                buy = parts[1].toInt(),
+                get = parts[2].toInt(),
+                start_date = dateFormat.parse(parts[3]),
+                end_date = dateFormat.parse(parts[4])
+            )
+        }
     }
 }
 
-//fun readPromotionCsv(filePath: String): List<Promotion> {
-//    val lines = Files.readAllLines(Paths.get(filePath))
-//    return lines.drop(1).map { line ->
-//        val values = line.split(",")
-//        Promotion(
-//            name = values[0],
-//            buy = values[1].toInt(),
-//            get = values[2].toInt(),
-//            start_date = values[3],
-//            end_date = values[4]
-//        )
-//    }
-//}
+
 
