20d9cc8433a1c16649978636b3b97b79b2b9794f
김의현 <hyunkim6790@gmail.com>
2024-11-30 23:54:19 +0900

    Style: 구현해야 할 기능 주석 추가

--- src/main/kotlin/store/service/Checkout.kt
+++ src/main/kotlin/store/service/Checkout.kt
@@ -1,4 +1,10 @@
 package store.service
 
 class Checkout {
+    // TODO: 프로모션 함수 추가
+    fun promotionDiscount() {}
+
+    fun membershipDiscount() {
+        // (총 금액 - 프로모션 할인) * 30 / 100
+    }
 }
--- src/main/kotlin/store/service/Inventory.kt
+++ src/main/kotlin/store/service/Inventory.kt
@@ -1,4 +1,11 @@
 package store.service
 
-class Inventory {
+import store.model.Products
+
+class Inventory(private val products: MutableList<Products>) {
+    fun getProduct(name:String): Products? {
+        return products.find { it.name == name }
+    }
+
+    // TODO: 재고 차감 함수 구현하기
 }
