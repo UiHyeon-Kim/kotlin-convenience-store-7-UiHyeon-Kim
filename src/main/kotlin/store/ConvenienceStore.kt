9b5cc68dadde3d7a9c9e227a61fa659d911a2f94
김의현 <hyunkim6790@gmail.com>
2024-11-30 22:26:30 +0900

    feat(ConvenienceStore): 여러 함수 구현
    - 영수증 출력 함수 구현
    - 재고 및 수량 검증 함수 구현
    - 추가 구매 함수 구현

--- src/main/kotlin/store/ConvenienceStore.kt
+++ src/main/kotlin/store/ConvenienceStore.kt
@@ -1,30 +1,75 @@
 package store
 
 class ConvenienceStore(
-    val products: List<Products>,
-    val inputView: InputView,
-    val outputView: OutputView,
+    private val products: List<Products>,
+    private val promotion: List<Promotion>,
+    private val inputView: InputView,
+    private val outputView: OutputView,
 ) {
     fun start() {
         outputView.printWelcomeMessage()
         outputView.printProducts(products)
-        val rawProductAndQuantity = inputView.readProductAndQuantity()
-        val productsAndQuantities = validateProductAndQuantity(rawProductAndQuantity)
-        productsAndQuantities.forEach { pair ->
-            println("${pair.first} ${pair.second}")
+        val productsAndQuantities = validateProductAndQuantity()
+
+        val purchaseInfo = products.map { it.name }
+        val pui = productsAndQuantities.map { it.first in purchaseInfo }
+        println(pui)
+
+        val asd = productsAndQuantities.map { pair ->
+            products.forEach { product ->
+                product.name
+            }
         }
 
+
+        readMembershipDiscount()
+        printReceipt()
+        readMorePurchase()
     }
 
-    // TODO: 예외처리 넣기
-    fun validateProductAndQuantity(rawProductAndQuantity: String): List<Pair<String, Int>> {
+    private fun validateProductAndQuantity(): List<Pair<String, Int>> = retryInput {
+        val rawProductAndQuantity = inputView.readProductAndQuantity()
         val splitProductsAndQuantities = rawProductAndQuantity.split(",")
+        val productsAndQuantities = validateFormat(splitProductsAndQuantities)
+        productsAndQuantities
+    }
+
+    private fun validateFormat(splitProductsAndQuantities: List<String>): List<Pair<String, Int>> {
         val productsAndQuantities = splitProductsAndQuantities.map { item ->
             val product = item.trim('[', ']').split("-")[0]
             val quantities = item.trim('[', ']').split("-")[1]
-            product to quantities.toInt()
+            require(product in products.map { it.name }) { "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요." }
+            product to requireNotNull(quantities.toIntOrNull()) { "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요." }
         }
         return productsAndQuantities
     }
 
+    private fun readMembershipDiscount() = retryInput {
+        val choice = inputView.readMembershipDiscount()
+        when (choice.uppercase()) {
+            "Y" -> println() //TODO: 멤버십 할인 함수 넣기
+            "N" -> println()
+            else -> throw IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.")
+        }
+    }
+
+    private fun printReceipt(
+//        purchaseInfo: List<PurchaseInfo>,
+    ) {
+        outputView.printReceiptHeader()
+//        outputView.printPurchaseInfo(purchaseInfo)
+        outputView.printPromotionHeader()
+        outputView.printPromotionInfo()
+        outputView.printTotalHeader()
+//        outputView.printTotalInfo()
+    }
+
+    private fun readMorePurchase() = retryInput {
+        val choice = inputView.readMorePurchase()
+        when (choice.uppercase()) {
+            "Y" -> start()
+            "N" -> return@retryInput
+            else -> throw IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.")
+        }
+    }
 }
