ac8c64d1facd8de775fe97642778420fe8e2d118
김의현 <hyunkim6790@gmail.com>
2024-11-30 14:37:41 +0900

    feat(FileManager): 재고 불러오는 함수 구현

---
+++ src/main/kotlin/store/FileManager.kt
@@ -0,0 +1,19 @@
+package store
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun readProductsCsv(filePath: String): List<Products> {
+    val lines = Files.readAllLines(Paths.get(filePath))
+
+    return lines.drop(1).map { line ->
+        val values = line.split(",")
+        Products(
+            name = values[0],
+            price = values[1].toInt(),
+            quantity = values[2].toInt(),
+            promotion = values[3].ifEmpty { null }
+        )
+    }
+}
+
