aea48c0881e51aa3ecac98a55427518832936f59
김의현 <hyunkim6790@gmail.com>
2024-11-30 14:51:24 +0900

    feat(OutputView): 재고 출력 함수 구현

--- src/main/kotlin/store/OutputView.kt
+++ src/main/kotlin/store/OutputView.kt
@@ -4,4 +4,14 @@ class OutputView {
     fun printWelcomeMessage() {
         println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.")
     }
+
+    fun printProducts(products: List<Products>) {
+        products.forEach { product ->
+            if (product.quantity < 0) {
+                println("- ${product.name} ${product.price}원 재고 없음 ${product.promotion}")
+            } else {
+                println("- ${product.name} ${product.price}원 ${product.quantity}개 ${product.promotion}")
+            }
+        }
+    }
 }
