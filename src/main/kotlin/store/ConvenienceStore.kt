e28f29f3ebb0bb7393668d56d8ae777704f20c81
김의현 <hyunkim6790@gmail.com>
2024-11-30 15:39:12 +0900

    feat(ConvenienceStore): 상품과 수량 구분 함수 구현

--- src/main/kotlin/store/ConvenienceStore.kt
+++ src/main/kotlin/store/ConvenienceStore.kt
@@ -9,6 +9,22 @@ class ConvenienceStore(
         outputView.printWelcomeMessage()
         outputView.printProducts(products)
         val rawProductAndQuantity = inputView.readProductAndQuantity()
+        val productsAndQuantities = validateProductAndQuantity(rawProductAndQuantity)
+        productsAndQuantities.forEach { pair ->
+            println("${pair.first} ${pair.second}")
+        }
 
     }
+
+    // TODO: 예외처리 넣기
+    fun validateProductAndQuantity(rawProductAndQuantity: String): List<Pair<String, Int>> {
+        val splitProductsAndQuantities = rawProductAndQuantity.split(",")
+        val productsAndQuantities = splitProductsAndQuantities.map { item ->
+            val product = item.trim('[', ']').split("-")[0]
+            val quantities = item.trim('[', ']').split("-")[1]
+            product to quantities.toInt()
+        }
+        return productsAndQuantities
+    }
+
 }
