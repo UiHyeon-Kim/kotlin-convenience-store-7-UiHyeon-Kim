0809e0d950926b12efd9afc2272000f24f08ed49
김의현 <hyunkim6790@gmail.com>
2024-11-30 16:05:40 +0900

    feat(Promotion): 프로모션 데이터 쿨래스 구현

--- src/main/kotlin/store/FileManager.kt
+++ src/main/kotlin/store/FileManager.kt
@@ -5,7 +5,6 @@ import java.nio.file.Paths
 
 fun readProductsCsv(filePath: String): List<Products> {
     val lines = Files.readAllLines(Paths.get(filePath))
-
     return lines.drop(1).map { line ->
         val values = line.split(",")
         Products(
@@ -16,4 +15,18 @@ fun readProductsCsv(filePath: String): List<Products> {
         )
     }
 }
+
+//fun readPromotionCsv(filePath: String): List<Promotion> {
+//    val lines = Files.readAllLines(Paths.get(filePath))
+//    return lines.drop(1).map { line ->
+//        val values = line.split(",")
+//        Promotion(
+//            name = values[0],
+//            buy = values[1].toInt(),
+//            get = values[2].toInt(),
+//            start_date = values[3],
+//            end_date = values[4]
+//        )
+//    }
+//}
 
---
+++ src/main/kotlin/store/Promotion.kt
@@ -0,0 +1,5 @@
+package store
+
+import java.util.Date
+
+data class Promotion(val name:String, val buy:Int, val get:Int, val start_date: Date, val end_date: Date)
