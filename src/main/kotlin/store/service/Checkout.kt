dc0d686461b93db58333ff1f85a306eb311fb0ed
김의현 <hyunkim6790@gmail.com>
2024-12-03 21:57:28 +0900

    feat(Checkout): 멤버십 할인 함수 구현

--- src/main/kotlin/store/service/Checkout.kt
+++ src/main/kotlin/store/service/Checkout.kt
@@ -1,10 +1,22 @@
 package store.service
 
-class Checkout {
-    // TODO: 프로모션 함수 추가
-    fun promotionDiscount() {}
+import store.model.Products
+import store.model.Promotion
+import store.model.ShoppingCart
+import java.time.LocalDate
 
-    fun membershipDiscount() {
+class Checkout(private val promotions: List<Promotion>) {
+    // TODO: 프로모션 함수 추가
+    fun promotionDiscount(product: Products) {
+        val promotion = promotions.find {
+            it.name == product.promotion && LocalDate.now() in it.startDate..it.endDate
+        }
+    }
+
+    fun membershipDiscount(cartItems: List<ShoppingCart>): Int {
         // (총 금액 - 프로모션 할인) * 30 / 100
+        val totalPrice = cartItems.sumOf { it.price * it.quantity }
+        val discount = (totalPrice * 0.3).toInt()
+        return if (discount > 8000) 8000 else discount
     }
 }
