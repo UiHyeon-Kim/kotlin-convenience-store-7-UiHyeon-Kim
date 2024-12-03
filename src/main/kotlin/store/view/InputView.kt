55db12ab147a8ec54d593917c3de0b5e2fa9c6d8
김의현 <hyunkim6790@gmail.com>
2024-12-03 22:00:50 +0900

    refactor(InputView): 입력함수 리팩터링

--- src/main/kotlin/store/view/InputView.kt
+++ src/main/kotlin/store/view/InputView.kt
@@ -9,15 +9,21 @@ class InputView {
         return Console.readLine()
     }
 
-    fun readAddPromotion(): String {
+    fun readSelect(message: String): String {
         println()
-        println()
+        println(message)
         return Console.readLine()
     }
 
-    fun readNonDiscountPromotion(): String {
+    /*fun readAddPromotion(name: String): String {
         println()
+        println("현재 ${name}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)")
+        return Console.readLine()
+    }
+
+    fun readNonDiscountPromotion(name: String, quantity: String): String {
         println()
+        println("현재 $name ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)")
         return Console.readLine()
     }
 
@@ -25,7 +31,7 @@ class InputView {
         println()
         println("멤버십 할인을 받으시겠습니까? (Y/N)")
         return Console.readLine()
-    }
+    }*/
 
     fun readMorePurchase(): String {
         println()
