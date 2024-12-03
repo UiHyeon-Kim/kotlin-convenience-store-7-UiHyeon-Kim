70900a3cef6af7987a0cd9ad4a10c64ec917ff56
김의현 <hyunkim6790@gmail.com>
2024-12-03 22:02:55 +0900

    refactor(ConvenienceStore): Y/N 입력함수 방식 변경 및 영수증 출력

--- src/main/kotlin/store/ConvenienceStore.kt
+++ src/main/kotlin/store/ConvenienceStore.kt
@@ -22,40 +22,56 @@ class ConvenienceStore(
         outputView.printProducts(products)
         val cartItems = parseAndValidateCartItems()
         //TODO: 프로모션 적용 함수 추가하기
-        choiceMembershipDiscount()
-        printReceipt()
+        if (isSelect("멤버십 할인을 받으시겠습니까? (Y/N)")) checkout.membershipDiscount(cartItems)
+        printReceipt(cartItems, 300, 300)
         choiceMorePurchase()
     }
 
     private fun parseAndValidateCartItems(): List<ShoppingCart> = retryInput {
         val rawCartItems = inputView.readProductAndQuantity()
-        val splitCartItems = rawCartItems.split(",")
-        return@retryInput splitCartItems.map { item ->
-            val (product, quantityString) = item.trim('[', ']').split("-")
+        return@retryInput rawCartItems.split(",").map { item ->
+            val (productName, quantityString) = item.trim('[', ']').split("-")
+            val product = requireNotNull(inventory.getProduct(productName)) { "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요." }
             val quantity = requireNotNull(quantityString.toIntOrNull()) {"[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."}
-            require(product in products.map { it.name }) { "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요." }
             require(quantity > 0) {"[ERROR] 상품은 1개 이상 구매해야 합니다. 다시 입력해 주세요."}
-            ShoppingCart(product, quantity)
+            //TODO: 재고 부족할 시 에러 처리
+            products.filter { it.name == productName }.sumOf { it.quantity } >= quantity
+            //TODO: 일반재고만 없는 아이템 채우기
+            ShoppingCart(
+                name = product.name,
+                quantity = quantity,
+                price = product.price,
+                promotionStock = product.quantity,
+            )
         }
     }
 
-    private fun choiceMembershipDiscount() = retryInput {
+    /*fun choiceMembershipDiscount(cartItems: List<ShoppingCart>) = retryInput {
         val choice = inputView.readMembershipDiscount()
         when (choice.uppercase()) {
-            "Y" -> println() //TODO: 멤버십 할인 함수 넣기
+            "Y" -> checkout.membershipDiscount(cartItems)
             "N" -> println()
+            else -> throw IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.")
+        }
+    }*/
+
+    private fun isSelect(message: String) = retryInput {
+        val choice = inputView.readSelect(message)
+        when (choice.uppercase()) {
+            "Y" -> return@retryInput true
+            "N" -> return@retryInput false
             else -> throw IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.")
         }
     }
 
     private fun printReceipt(
-//        purchaseInfo: List<ShoppingCart>,
-//        promotionDiscount: Int,
-//        membershipDiscount: Int,
+        cartItems: List<ShoppingCart>,
+        promotionDiscount: Int,
+        membershipDiscount: Int,
     ) {
-//        outputView.printPurchaseInfo(purchaseInfo)
-//        outputView.printPromotionInfo()
-//        outputView.printTotalInfo()
+        outputView.printPurchaseInfo(cartItems)
+        outputView.printPromotionInfo("오렌지 주스", 20)
+        outputView.printTotalInfo(10,100,1000,10000,100000)
     }
 
     private fun choiceMorePurchase() = retryInput {
